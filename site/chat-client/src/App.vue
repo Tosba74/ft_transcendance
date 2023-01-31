b6a5babdaa935aa05ea06004ec677c656022123d
Thomas Nanchen <thomasnanchen@hotmail.com>
2023-01-31 16:29:37 +0100

    module de chat ameliore

--- site/chat-client/src/App.vue
+++ site/chat-client/src/App.vue
@@ -1,19 +1,56 @@
 <script setup>
 import { io } from 'socket.io-client';
+
+// vue working-process: https://learnvue.co/tutorials/vue-lifecycle-hooks-guide
 import { onBeforeMount, ref } from 'vue';
 
 const socket = io('http://localhost:4000');
 
+const usersCount = ref('');
+const users = ref([]);
+// const users = ref('');
 const messages = ref([]);
 const messageText = ref('');
 const joined = ref(false);
 const name = ref('');
 const typingDisplay = ref('');
 
-// EVENTS LIES AUX AUTRES SOCKETS (on)
+/*
+	emit(event, bodyarguments, optional_callback)
+	suite a une interaction avec la page html,
+	mon socket emit un message au server (requete GET POST selon bodyarguments)
+
+	on(event, optional_callback)
+	suite a un event emit par le server
+	mon socket recoit un message du server suite
+
+	!!! optional_callback appelée que si l'event coté server retourne une valeur
+*/
+
+// Called right before the component DOM is actually rendered and mounted
 onBeforeMount(() => {
+	socket.emit('findAllUsers', {}, (response) => {
+		console.log(response);
+		users.value = response;
+	})
+
 	socket.emit('findAllMessages', {}, (response) => {
+		console.log(response);
 		messages.value = response;
+	})
+
+	socket.on('users', (count) => {
+		if (count > 1) {
+			usersCount.value = `${count} users connected`;
+		}
+		else {
+			usersCount.value = `${count} user connected`;
+		}
+	})
+
+	socket.on('join', (user) => {
+		console.log(users);
+		users.value.push(user);
 	})
 
 	socket.on('message', (message) => {
@@ -27,14 +64,16 @@ onBeforeMount(() => {
 		else {
 			typingDisplay.value = '';
 		}
-	});
+	})
 });
 
-// EVENTS LIES A MON SOCKET (emit)
 const join = () => {
-	socket.emit('join', {name: name.value}, () => {
-		joined.value = true;
-	})
+	if (name.value.length > 0)
+	{
+		socket.emit('join', { name: name.value }, () => {
+			joined.value = true;
+		})
+	}
 };
 
 const sendMessage = () => {
@@ -43,7 +82,6 @@ const sendMessage = () => {
 	})
 };
 
-// let timeout;
 const emitTyping = () => {
 	socket.emit('typing', { isTyping: true});
 	let timeout = setTimeout (() => {
@@ -55,6 +93,7 @@ const emitTyping = () => {
 <template>
 	<div class ="chat">
 		
+		<!-- MODULE D'IDENTIFICATION -->
 		<div v-if="!joined">
 			<form @submit.prevent="join">
 				<label>What's your name?</label>
@@ -62,20 +101,29 @@ const emitTyping = () => {
 				<button type="submit">Send</button>
 			</form>
 		</div>
-
+		
+		<!-- MODULE DE CHATROOM -->
 		<div class="chat-container" v-else>
-			<div class="messages-container">
-				<div v-for="message in messages">
-					[{{ message.name}}]: {{  message.text }}
-				</div>
+			<div class="users-container">
+				<div class="count">{{ usersCount }}</div>
+				<div v-for="user in users">{{ user }}</div>
 			</div>
-			<div class="message-input">
-				<div v-if="typingDisplay">{{ typingDisplay }}</div>
-				<form @submit.prevent="sendMessage">
-					<label>Message:</label>
-					<input v-model="messageText" @input="emitTyping" />
-					<button type="submit">Send</button>
-				</form>
+			<div class="messages-container">
+				<div class="messages">
+					<div v-for="message in messages">
+						[{{ message.name }}]: {{  message.text }}
+					</div>
+				</div>
+				<div class="write">
+					<div class="typing">
+						<div v-if="typingDisplay">{{ typingDisplay }}</div>
+					</div>
+					<form @submit.prevent="sendMessage">
+						<label>Message:</label>
+						<input v-model="messageText" @input="emitTyping" />
+						<button type="submit">Send</button>
+					</form>
+				</div>
 			</div>
 		</div>
 
@@ -85,19 +133,43 @@ const emitTyping = () => {
 <style>
 @import './assets/base.css';
 
+#app {
+	display: flex !important;
+	justify-content: center;
+	width: 100%;
+}
+
 .chat {
 	padding:20px;
 	height: 100vh;
+	width: 800px;
 }
 
 .chat-container {
 	display: flex;
-	flex-direction: column;
-	height: 100%;
+	flex-direction: row;
 	justify-content: space-between;
+	width: 100%;
+	height: 100%;
 }
 
-.message-container {
-	flex: 1;
+.users-container {
+	width: 25%;
+	text-align: right;
+	padding: 0 10px;
 }
+
+.messages-container {
+	display: flex;
+	flex-direction: column;
+	justify-content: space-between;
+	width: 75%;
+	padding: 0 10px;
+}
+
+.count {
+	text-decoration: underline;
+	padding-bottom: 5px;
+}
+
 </style>
