4545b239922d3455100bf5ea7a568275ef3b0d13
Thomas Nanchen <thomasnanchen@hotmail.com>
2023-02-09 17:59:15 +0100

    tester docker sur autre ordi

--- _build/back/start.sh
+++ _build/back/start.sh
@@ -3,14 +3,17 @@ apk add nano
 
 # sleep 1000000
 
-yarn global add @nestjs/cli
+# yarn global add @nestjs/cli										# slow docker
 
 # # Create application
 if [ "$BUILD_TYPE" = "Setup" ]; 
 then 
-    nest new ./ -p yarn --strict
+    # nest new ./ -p yarn --strict
 
-    yarn add @nestjs/websockets @nestjs/platform-socket.io
+    # yarn add @nestjs/websockets @nestjs/platform-socket.io
+	yarn install
+	yarn add @nestjs/websockets@9.2.1 @nestjs/platform-socket.io@9.2.1
+	yarn add @nestjs/typeorm @nestjs/swagger 
 fi
 
 
@@ -20,7 +23,7 @@ fi
 # json-server ./api/db.json --host 0.0.0.0
 
 # Download node_modules
-yarn install
+# yarn install
 
 
 if [ "$BUILD_TYPE" = "Production" ]; 
