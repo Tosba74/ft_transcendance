d260097befd6b029ecdab2547f12f51ca1d0ef00
Tosba74 <bastm.ua42@gmail.com>
2023-02-26 22:26:51 +0100

    profile button

--- site/src/components/Profile/ProfileButton.tsx
+++ site/src/components/Profile/ProfileButton.tsx
@@ -10,12 +10,12 @@ export default function ProfileButton() {
 	
 	return (
 		<div className="flex items-center md:order-2">
-    	    <button type="button" className="flex mr-3 text-sm bg-gray-800 rounded-full md:mr-0 focus:ring-4 focus:ring-gray-300 dark:focus:ring-gray-600" id="user-menu-button" aria-expanded="false" data-dropdown-toggle="user-dropdown" data-dropdown-placement="bottom">
+    	    <button type="button" onClick={handleClick} className="flex mr-3 text-sm bg-gray-800 rounded-full md:mr-0 focus:ring-4 focus:ring-gray-300 dark:focus:ring-gray-600" id="user-menu-button" aria-expanded="false" data-dropdown-toggle="user-dropdown" data-dropdown-placement="bottom">
     	        <span className="sr-only">Open user menu</span>
     	        <img className="w-8 h-8 rounded-full" height="8" src={LogoInconnu} alt="user photo" />
     	    </button>
     	    {
-    	    isOpen&&  <div className="hidden absolute top-2 right-2 z-40 w-40 text-base list-none bg-white divide-y divide-gray-100 rounded-lg shadow-lg dark:bg-gray-700 dark:divide-gray-600" id="user-dropdown">
+    	    isOpen&&  <div className="absolute top-16 right-2 z-40 w-40 text-base list-none bg-white divide-y divide-gray-100 rounded-lg shadow-lg dark:bg-gray-700 dark:divide-gray-600" id="user-dropdown">
     	        <div className="px-4 py-3">
     	            <span className="block text-sm text-gray-900 dark:text-white">Login_42</span>
     	            <span className="block text-sm font-medium text-gray-500 truncate dark:text-gray-400">name@gmail.com</span>
