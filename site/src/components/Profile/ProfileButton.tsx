31a9528ba8e853909adb213873afe868da46292e
Bast <bastm.ua42@gmail.com>
2023-02-26 14:24:15 +0100

    navbar encore bug

--- site/src/components/Profile/ProfileButton.tsx
+++ site/src/components/Profile/ProfileButton.tsx
@@ -1,14 +1,21 @@
 import LogoInconnu from './inconnu.jpeg'
+import { useState } from 'react';
 
 export default function ProfileButton() {
+	const [isOpen, setOpen] = useState(false);
+
+	const handleClick = () => {
+		setOpen(!isOpen);
+	};
+	
 	return (
 		<div className="flex items-center md:order-2">
     	    <button type="button" className="flex mr-3 text-sm bg-gray-800 rounded-full md:mr-0 focus:ring-4 focus:ring-gray-300 dark:focus:ring-gray-600" id="user-menu-button" aria-expanded="false" data-dropdown-toggle="user-dropdown" data-dropdown-placement="bottom">
     	        <span className="sr-only">Open user menu</span>
     	        <img className="w-8 h-8 rounded-full" height="8" src={LogoInconnu} alt="user photo" />
     	    </button>
-    	    {/* <!-- Dropdown menu --> */}
-    	    <div className="hidden absolute top-2 right-2 z-40 w-40 text-base list-none bg-white divide-y divide-gray-100 rounded-lg shadow-lg dark:bg-gray-700 dark:divide-gray-600" id="user-dropdown">
+    	    {
+    	    isOpen&&  <div className="hidden absolute top-2 right-2 z-40 w-40 text-base list-none bg-white divide-y divide-gray-100 rounded-lg shadow-lg dark:bg-gray-700 dark:divide-gray-600" id="user-dropdown">
     	        <div className="px-4 py-3">
     	            <span className="block text-sm text-gray-900 dark:text-white">Login_42</span>
     	            <span className="block text-sm font-medium text-gray-500 truncate dark:text-gray-400">name@gmail.com</span>
@@ -25,10 +32,8 @@ export default function ProfileButton() {
     	            </li>
     	        </ul>
     	    </div>
-    	    <button data-collapse-toggle="mobile-menu" type="button" className="inline-flex items-center p-2 ml-1 text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600" aria-controls="mobile-menu" aria-expanded="false">
-    	        <span className="sr-only">Open main menu</span>
-    	        <svg className="w-6 h-6" aria-hidden="true" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M3 5a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 10a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 15a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1z" clip-rule="evenodd"></path></svg>
-    	    </button>
+			}
     	</div>
 	);
 }
+
