895c9cf7adfb7542741e237dc184b59148c3d70a
Tosba74 <bastm.ua42@gmail.com>
2023-03-01 16:42:22 +0100

    debut de tchat

--- site/src/components/NavBar/BurgerButton.tsx
+++ site/src/components/NavBar/BurgerButton.tsx
@@ -1,8 +1,9 @@
 import React, { useState } from "react";
+import { Link } from "react-router-dom";
 import classNames from "classnames";
 
 export default function BurgerButton() {
-    const [isOpen, setOpen] = React.useState(false);
+    const [isOpen, setOpen] = useState(false);
 
     const handleClick = () => {
         setOpen(!isOpen);
@@ -15,45 +16,21 @@ export default function BurgerButton() {
                 <svg className="w-6 h-6" aria-hidden="true" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M3 5a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 10a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 15a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1z" clip-rule="evenodd"></path></svg>
             </button>
             <div className={classNames("absolute md:relative right-2 items-center sm:justify-between md:justify-between justify-end md:flex md:w-auto md:order-1 md:visible", isOpen ? "visible" : "invisible")}>
-            {/* <div className="sm:relative md:relative absolute items-center sm:justify-between md:justify-between justify-end w-full md:flex md:w-auto md:order-1" id="mobile-menu"> */}
-                    <ul className="flex flex-col p-4 mt-4 border border-cyan-500 rounded-lg bg-gray-50 md:flex-row md:space-x-8 md:mt-0 md:text-sm md:font-medium md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-cyan-500 md:visible">
-                        <li>
-                            <a href="/" className="block py-2 pl-3 pr-4 text-white bg-cyan-500 rounded md:bg-transparent md:text-cyan-500 md:p-0 dark:text-white" aria-current="page">Home</a>
-                        </li>
-                        <li>
-                            <a href="/game" className="block py-2 pl-3 pr-4 text-gray-700 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 dark:text-gray-400 md:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700">Game</a>
-                        </li>
-                        <li>
-                            <a href="/players" className="block py-2 pl-3 pr-4 text-gray-700 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 dark:text-gray-400 md:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700">Players</a>
-                        </li>
-                        <li>
-                            <a href="/login" className="block py-2 pl-3 pr-4 text-gray-700 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 dark:text-gray-400 md:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700">Login</a>
-                        </li>
-                    </ul>
-                {/* </div> */}
+                <ul className="flex flex-col p-4 mt-4 border border-cyan-500 rounded-lg bg-gray-50 md:flex-row md:space-x-8 md:mt-0 md:text-sm md:font-medium md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-cyan-500 md:visible">
+                    <Link to="/" className="block py-2 pl-3 pr-4 text-white bg-cyan-500 rounded md:bg-transparent md:text-cyan-500 md:p-0 dark:text-white" aria-current="page">
+                        Home
+                    </Link>
+                    <Link to="/game" className="block py-2 pl-3 pr-4 text-gray-700 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 dark:text-gray-400 md:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700">
+                        Game
+                    </Link>
+                    <Link to="/chat" className="block py-2 pl-3 pr-4 text-gray-700 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 dark:text-gray-400 md:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700">
+                        Chat
+                    </Link>
+                    <Link to="/players" className="block py-2 pl-3 pr-4 text-gray-700 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 dark:text-gray-400 md:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700">
+                        Players
+                    </Link>
+                </ul>
             </div>
         </div>
     );
 }
-// export default function BurgerButton() {
-// return (
-// <nav className="bg-white border-gray-200 px-2 sm:px-4 py-2.5 rounded dark:bg-gray-900">
-{/* <div className="items-center justify-between w-full md:flex md:w-auto md:order-1" id="mobile-menu"> */ }
-{/* <ul className="hidden absolute top-2 right-2 p-4 mt-4 border rounded-lg bg-gray-500 md:flex-row md:space-x-8 md:mt-0 md:text-sm md:font-medium md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-cyan-500"> */ }
-{/* <li> */ }
-{/* <a href="/home" className="block py-2 pl-4 pr-4 text-white rounded bg-cyan-500 rounded md:bg-transparent md:text-cyan-500 md:p-0 dark:text-white" aria-current="page">Home</a> */ }
-{/* </li> */ }
-{/* <li> */ }
-{/* <a href="/game" className="block py-2 pl-4 pr-4 text-gray-700 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 dark:text-gray-400 md:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700">Game</a> */ }
-{/* </li> */ }
-{/* <li> */ }
-{/* <a href="/players" className="block py-2 pl-4 pr-4 text-gray-700 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 dark:text-gray-400 md:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700">Players</a> */ }
-{/* </li> */ }
-{/* <li> */ }
-{/* <a href="/login" className="block py-2 pl-4 pr-4 text-gray-700 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 dark:text-gray-400 md:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700">Login</a> */ }
-{/* </li> */ }
-{/* </ul> */ }
-{/* </div> */ }
-{/* </nav> */ }
-	// );
-// }
--- site/src/components/NavBar/ProfileButton.tsx
+++ site/src/components/NavBar/ProfileButton.tsx
@@ -11,14 +11,11 @@ export default function ProfileButton() {
 	return (
 		<div className="flex items-between md:order-2">
     	    <button 
-			type="button"
-			onClick={handleClick}
-			className="flex mr-3 text-sm bg-gray-800 rounded-full md:mr-0 focus:ring-4 focus:ring-gray-300 dark:focus:ring-gray-600" id="user-menu-button" aria-expanded="false" data-dropdown-toggle="user-dropdown" data-dropdown-placement="bottom">
+			type="button" onClick={handleClick} className="flex justify-items-end mr-3 text-sm bg-gray-800 rounded-full md:mr-0 focus:ring-4 focus:ring-gray-300 dark:focus:ring-gray-600" id="user-menu-button" aria-expanded="false" data-dropdown-toggle="user-dropdown" data-dropdown-placement="bottom">
     	        <span className="sr-only">Open user menu</span>
     	        <img className="w-8 h-8 rounded-full" height="8" src={LogoInconnu} alt="user photo" />
     	    </button>
-    	    {
-    	    isOpen&&  <div className="absolute top-20 right-2 z-40 w-40 text-base list-none bg-white divide-y divide-gray-100 rounded-lg shadow-lg dark:bg-gray-700 dark:divide-gray-600" id="user-dropdown">
+    	    { isOpen&&  <div className="absolute top-20 right-2 z-40 w-40 text-base list-none bg-white divide-y divide-gray-100 rounded-lg shadow-lg dark:bg-gray-700 dark:divide-gray-600" id="user-dropdown">
     	        <div className="px-4 py-3">
     	            <span className="block text-sm text-gray-900 dark:text-white">Login_42</span>
     	            <span className="block text-sm font-medium text-gray-500 truncate dark:text-gray-400">name@gmail.com</span>
@@ -34,7 +31,7 @@ export default function ProfileButton() {
     	                <a href="/login" className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 dark:hover:bg-gray-600 dark:text-gray-200 dark:hover:text-white">Log in</a>
     	            </li>
     	            <li>
-    	                <a href="/signout" className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 dark:hover:bg-gray-600 dark:text-gray-200 dark:hover:text-white">Sign out</a>
+    	                <a href="/logout" className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 dark:hover:bg-gray-600 dark:text-gray-200 dark:hover:text-white">Log out</a>
     	            </li>
     	        </ul>
     	    </div>
@@ -43,3 +40,4 @@ export default function ProfileButton() {
 	);
 }
 
+{/* isOpen && <div className="z-50 hidden my-4 text-base list-none bg-white divide-y divide-gray-100 rounded-lg shadow dark:bg-gray-700 dark:divide-gray-600" id="user-dropdown"> */}
