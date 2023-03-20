894ccc58df4435be5a84de425ff8d7d83766caed
Thomas Nanchen <thomasnanchen@hotmail.com>
2023-03-20 13:18:57 +0100

    remis les fichiers des components de base (handsonreact)

--- site/src/components/Profile/ProfilePage.tsx
+++ site/src/components/Profile/ProfilePage.tsx
@@ -1,3 +1,7 @@
+// import { Mock_Profile } from "./Mock_Profile";
+// import ProfileList from "./ProfileList";
+// import { Profile } from "./Profile";
+
 import Avatar from "./Avatar";
 import Pseudo from "./Pseudo";
 import ProfilePublic from "./ProfilePublic";
@@ -11,13 +15,24 @@ interface ProfilePageProps {
 
 export default function ProfilePage({user, refreshUserInfos}: ProfilePageProps) {
 
+    // const [profiles, setProfiles] = useState<Profile[]>(Mock_Profile);
+
+    // const saveProfile = (profile: Profile) => {
+    //     let updatedProfiles = profiles.map((p: Profile) => {
+    //         return p.id === profile.id ? profile : p;
+    //     });
+    //     setProfiles(updatedProfiles);
+    // };
+
     return (
         <>
         <div>
-            <h2>Your profile</h2>            
+            <h2>Profiles !!</h2>
+            {/* <ProfileList onSave={saveProfile} profiles={profiles} />             */}
+            
+            
             <Pseudo user={user} refreshUserInfos={refreshUserInfos}/>
             <Avatar user={user} refreshUserInfos={refreshUserInfos}/>
-
             {/* add informations like stats, friend, channels, etc in the next 2 components */}
             {/* <ProfilePublic user={user} /> */}
             {/* <ProfilePrivate user={user} /> */}
