c1c17449e2aa60fa2e4e7243657c63b896e2b561
tpinto-m <tpinto-m@student.42lausanne.ch>
2023-04-04 20:38:00 +0200

    add fade in and out animation

--- site/tailwind.config.js
+++ site/tailwind.config.js
@@ -2,7 +2,25 @@
 module.exports = {
   content: ["./src/**/**/*.{tsx, ts}"],
   theme: {
-    extend: {},
+    extend: {
+      // that is animation class
+      animation: {
+        fadeOut: 'fadeOut 0.5s ease-in-out',
+        fadeIn: 'fadeIn 0.5s ease-in-out',
+      },
+
+      // that is actual animation
+      keyframes: theme => ({
+        fadeOut: {
+          '0%': { opacity: theme('opacity.100') },
+          '100%': { opacity: theme('opacity.0') },
+        },
+        fadeIn: {
+          '0%': { opacity: theme('opacity.0') },
+          '100%': { opacity: theme('opacity.100') },
+        },
+      }),
+    },
   },
   plugins: [],
 }
