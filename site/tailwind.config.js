ae957e3f1555b218db18dfbadf2dbb8896db4830
tpinto-m <tpinto-m@student.42lausanne.ch>
2023-04-05 18:26:49 +0200

    add animation slide in out

--- site/tailwind.config.js
+++ site/tailwind.config.js
@@ -7,6 +7,8 @@ module.exports = {
       animation: {
         fadeOut: 'fadeOut 0.5s ease-in-out',
         fadeIn: 'fadeIn 0.5s ease-in-out',
+        slideInChat: 'slideInChat 0.5s ease-in-out',
+        slideOutChat: 'slideOutChat 0.5s ease-in-out',
       },
 
       // that is actual animation
@@ -18,6 +20,28 @@ module.exports = {
         fadeIn: {
           '0%': { opacity: theme('opacity.0') },
           '100%': { opacity: theme('opacity.100') },
+        },
+        slideInChat: {
+          '0%': {
+            opacity: theme('opacity.0'),
+            transform: 'translateX(100vh)'
+          },
+          '1%': { opacity: theme('opacity.100') },
+          '100%': {
+            opacity: theme('opacity.100'),
+            transform: 'translateX(0)'
+          },
+        },
+        slideOutChat: {
+          '0%': {
+            opacity: theme('opacity.100'),
+            transform: 'translateX(0)'
+          },
+          '1%': { opacity: theme('opacity.100') },
+          '100%': {
+            opacity: theme('opacity.0'),
+            transform: 'translateX(100vh)'
+          },
         },
       }),
     },
