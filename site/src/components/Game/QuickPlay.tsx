2fd3f7783e485e545537898d4b498b066fd6192e
Deegoh <tiagopintomonteiro@gmail.com>
2023-04-09 18:08:56 +0200

    add modal option

--- site/src/components/Game/QuickPlay.tsx
+++ site/src/components/Game/QuickPlay.tsx
@@ -2,29 +2,49 @@ import React, { useEffect } from "react";
 import { UseLoginDto } from "../Log/dto/useLogin.dto";
 import { createPortal } from "react-dom";
 import { useNavigate } from "react-router-dom";
+import classNames from "classnames";
 
 const startEL = document.getElementById("root");
 
 export default function QuickPlay({ loginer }: { loginer: UseLoginDto }) {
   const [portal, setPortal] = React.useState(false);
+  const [effect, setEffect] = React.useState(false);
+  const [mode, setMode] = React.useState("");
   const ref = React.useRef<HTMLDivElement | null>(null);
   const navigate = useNavigate();
 
-  const handleClickStart = () => {
-    console.log("create game");
+  const css = {
+    top: `${document.getElementById("startPage")?.offsetTop}px`
+  }
+
+  const handleClickSearch = () => {
+    setEffect(false);
+    // find game
+    if (mode)
+    {
+      console.log("so much fun");
+    }
+    else
+    {
+      console.log("Boomer");
+    }
     // navigate("/game");
   };
 
   const handleClickPortal = () => {
-    console.log("hello portal");
+    setEffect(true);
     setPortal(true);
+  };
+
+  const handleOnChange = (e:any) => {
+    setMode(e.target.checked)
   };
 
   useEffect(() => {
     const checkIfClickedOutside = (e: any) => {
       if (ref) {
         if (portal && !ref.current?.contains(e.target)) {
-          setPortal(false);
+          setEffect(false);
         }
       }
     };
@@ -48,24 +68,32 @@ export default function QuickPlay({ loginer }: { loginer: UseLoginDto }) {
         startEL !== null &&
         createPortal(
           <div
+          style={css}
             ref={ref}
-            className="absolute top-1/4 left-1/2 flex w-full max-w-md -translate-x-1/2 -translate-y-1/2 flex-col rounded-lg bg-red-600  p-8 shadow-lg"
+            className={classNames(
+              "flex flex-col max-w-md absolute left-1/2 w-3/4 -translate-x-1/2 rounded-lg bg-gray-100 p-4 text-center shadow-lg dark:bg-gray-700 dark:text-white sm:w-auto",
+              effect === false ? "animate-fadeOut opacity-0" : "opacity-1 animate-fadeIn"
+            )}
+            onAnimationEnd={() => {
+              if (!effect)
+                setPortal(false);
+            }}
           >
-            <label className="relative inline-flex cursor-pointer items-center">
-              <input type="checkbox" value="" className="peer sr-only" />
-              <div className="peer h-6 w-11 rounded-full bg-gray-200 after:absolute after:top-[2px] after:left-[2px] after:h-5 after:w-5 after:rounded-full after:border after:border-gray-300 after:bg-white after:transition-all after:content-[''] peer-checked:bg-blue-600 peer-checked:after:translate-x-full peer-checked:after:border-white peer-focus:outline-none peer-focus:ring-4 peer-focus:ring-blue-300 dark:border-gray-600 dark:bg-gray-700 dark:peer-focus:ring-blue-800"></div>
+            <h3 className="text-xl pb-2">Option</h3>
+
+            <label className="mx-auto relative inline-flex cursor-pointer items-center pb-2">
+              <input onChange={handleOnChange} type="checkbox" className="peer sr-only" />
+              <div className="peer h-6 w-11 rounded-full dark:bg-gray-800 bg-gray-200 after:absolute after:top-[2px] after:left-[2px] after:h-5 after:w-5 after:rounded-full after:border after:border-gray-300 after:bg-white after:transition-all after:content-[''] peer-checked:bg-blue-600 peer-checked:after:translate-x-full peer-checked:after:border-white peer-focus:outline-none peer-focus:ring-4 peer-focus:ring-blue-300 dark:border-gray-600 dark:peer-focus:ring-blue-800"></div>
               <span className="ml-3 text-sm font-medium text-gray-900 dark:text-gray-300">
                 Fun mode
               </span>
             </label>
-            <label htmlFor="">
-              <input type="text" />
-            </label>
+
             <button
-              onClick={handleClickStart}
+              onClick={handleClickSearch}
               className="my-2 mx-auto rounded-lg bg-cyan-500 px-4 py-2 text-center text-sm font-medium text-white hover:bg-blue-800 focus:outline-none focus:ring-4 focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
             >
-              Start
+              Search Game
             </button>
           </div>,
           startEL
