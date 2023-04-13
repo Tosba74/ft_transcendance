0cc95c73306e1c44ee2d8c44230f38677e1693f2
Thomas Nanchen <thomasnanchen@hotmail.com>
2023-04-13 19:03:00 +0200

    css ok

--- site/src/components/Chat/OwnerCommands.tsx
+++ site/src/components/Chat/OwnerCommands.tsx
@@ -14,92 +14,92 @@ import OwnerPw from "./OwnerPw";
 const startEL = document.getElementById("root");
 
 interface OwnerCommandsProps {
-  loginer: UseLoginDto;
-  sendMessage: Function;
-  participants: ParticipantDto[];
-  role: string;
-  room: ChatRoomDto | undefined;
+	loginer: UseLoginDto;
+	sendMessage: Function;
+	participants: ParticipantDto[];
+	role: string;
+	room: ChatRoomDto | undefined;
 }
 
 export default function OwnerCommands({
-  loginer,
-  sendMessage,
-  participants,
-  role,
-  room,
+	loginer,
+	sendMessage,
+	participants,
+	role,
+	room,
 }: OwnerCommandsProps) {
-  const [portal, setPortal] = React.useState(false);
-  const [effect, setEffect] = React.useState(false);
+	const [portal, setPortal] = React.useState(false);
+	const [effect, setEffect] = React.useState(false);
 
-  const ref = React.useRef<HTMLDivElement | null>(null);
+	const ref = React.useRef<HTMLDivElement | null>(null);
 
-  const handleClickPortal = (event: SyntheticEvent) => {
-    event.preventDefault();
-    setEffect(true);
-    setPortal(true);
-  };
+	const handleClickPortal = (event: SyntheticEvent) => {
+		event.preventDefault();
+		setEffect(true);
+		setPortal(true);
+	};
 
-  React.useEffect(() => {
-    const checkIfClickedOutside = (e: any) => {
-      if (ref) {
-        if (portal && !ref.current?.contains(e.target)) {
-          setEffect(false);
-        }
-      }
-    };
+	React.useEffect(() => {
+		const checkIfClickedOutside = (e: any) => {
+			if (ref) {
+				if (portal && !ref.current?.contains(e.target)) {
+					setEffect(false);
+				}
+			}
+		};
 
-    document.addEventListener("mousedown", checkIfClickedOutside);
+		document.addEventListener("mousedown", checkIfClickedOutside);
 
-    return () => {
-      document.removeEventListener("mousedown", checkIfClickedOutside);
-    };
-  }, [portal]);
+		return () => {
+			document.removeEventListener("mousedown", checkIfClickedOutside);
+		};
+	}, [portal]);
 
-  return (
-    <>
-      <button className="pl-1" onClick={handleClickPortal}>
-        - <span className="hover:underline">{role} settings</span>
-		{/* <svg style={{color: "white"}} xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="ml-1 inline-block h-6 w-6" viewBox="0 0 16 16"> <path d="M2.678 11.894a1 1 0 0 1 .287.801 10.97 10.97 0 0 1-.398 2c1.395-.323 2.247-.697 2.634-.893a1 1 0 0 1 .71-.074A8.06 8.06 0 0 0 8 14c3.996 0 7-2.807 7-6 0-3.192-3.004-6-7-6S1 4.808 1 8c0 1.468.617 2.83 1.678 3.894zm-.493 3.905a21.682 21.682 0 0 1-.713.129c-.2.032-.352-.176-.273-.362a9.68 9.68 0 0 0 .244-.637l.003-.01c.248-.72.45-1.548.524-2.319C.743 11.37 0 9.76 0 8c0-3.866 3.582-7 8-7s8 3.134 8 7-3.582 7-8 7a9.06 9.06 0 0 1-2.347-.306c-.52.263-1.639.742-3.468 1.105z" fill="white"></path> </svg> */}
-        <svg
-          className="ml-1 inline-block h-6 w-6 relative -top-1"
-          xmlns="http://www.w3.org/2000/svg"
-          viewBox="0 0 24 24"
-        >
-          <g>
-            <path fill="none" d="M0 0h24v24H0z" />
-            <path d="M22 12h-2V5H4v13.385L5.763 17H12v2H6.455L2 22.5V4a1 1 0 0 1 1-1h18a1 1 0 0 1 1 1v8zm-7.855 7.071a4.004 4.004 0 0 1 0-2.142l-.975-.563 1-1.732.976.563A3.996 3.996 0 0 1 17 14.126V13h2v1.126c.715.184 1.353.56 1.854 1.071l.976-.563 1 1.732-.975.563a4.004 4.004 0 0 1 0 2.142l.975.563-1 1.732-.976-.563c-.501.51-1.14.887-1.854 1.071V23h-2v-1.126a3.996 3.996 0 0 1-1.854-1.071l-.976.563-1-1.732.975-.563zM18 20a2 2 0 1 0 0-4 2 2 0 0 0 0 4z" />
-          </g>
-        </svg>
-      </button>
+	return (
+		<>
+			<button className="pl-1" onClick={handleClickPortal}>
+				- <span className="hover:underline">{role} settings</span>
+				<svg
+					style={{ color: "white" }}
+					xmlns="http://www.w3.org/2000/svg"
+					className="ml-1 inline-block h-4 w-4"
+					viewBox="0 0 512 512"
+				>
+					<path
+						d="M256,176a80,80,0,1,0,80,80A80.24,80.24,0,0,0,256,176Zm172.72,80a165.53,165.53,0,0,1-1.64,22.34l48.69,38.12a11.59,11.59,0,0,1,2.63,14.78l-46.06,79.52a11.64,11.64,0,0,1-14.14,4.93l-57.25-23a176.56,176.56,0,0,1-38.82,22.67l-8.56,60.78A11.93,11.93,0,0,1,302.06,486H209.94a12,12,0,0,1-11.51-9.53l-8.56-60.78A169.3,169.3,0,0,1,151.05,393L93.8,416a11.64,11.64,0,0,1-14.14-4.92L33.6,331.57a11.59,11.59,0,0,1,2.63-14.78l48.69-38.12A174.58,174.58,0,0,1,83.28,256a165.53,165.53,0,0,1,1.64-22.34L36.23,195.54a11.59,11.59,0,0,1-2.63-14.78l46.06-79.52A11.64,11.64,0,0,1,93.8,96.31l57.25,23a176.56,176.56,0,0,1,38.82-22.67l8.56-60.78A11.93,11.93,0,0,1,209.94,26h92.12a12,12,0,0,1,11.51,9.53l8.56,60.78A169.3,169.3,0,0,1,361,119L418.2,96a11.64,11.64,0,0,1,14.14,4.92l46.06,79.52a11.59,11.59,0,0,1-2.63,14.78l-48.69,38.12A174.58,174.58,0,0,1,428.72,256Z"
+						fill="white">
+					</path>
+				</svg>
+			</button>
 
-      {portal &&
-        startEL !== null &&
-        createPortal(
-          <div
-            ref={ref}
-            className={classNames(
-              "absolute left-1/2 top-1/2 z-50 min-w-[250px] -translate-x-1/2 -translate-y-1/2 items-center rounded-lg bg-gray-100 p-6 dark:bg-gray-800 dark:text-white shadow-lg",
-              effect ? "opacity-1 animate-fadeIn" : "animate-fadeOut opacity-0"
-            )}
-            onAnimationEnd={() => {
-              if (!effect) setPortal(false);
-            }}
-          >
-            <h3 className="text-center text-xl">Owner settings</h3>
+			{portal &&
+				startEL !== null &&
+				createPortal(
+					<div
+						ref={ref}
+						className={classNames(
+							"absolute left-1/2 top-1/2 z-50 min-w-[250px] -translate-x-1/2 -translate-y-1/2 items-center rounded-lg bg-gray-100 p-6 dark:bg-gray-800 dark:text-white shadow-lg",
+							effect ? "opacity-1 animate-fadeIn" : "animate-fadeOut opacity-0"
+						)}
+						onAnimationEnd={() => {
+							if (!effect) setPortal(false);
+						}}
+					>
+						<h3 className="text-center text-xl">Owner settings</h3>
 
-            {(room?.type === 3 && (
-              <OwnerInvite
-                loginer={loginer}
-                sendMessage={sendMessage}
-                participants={participants}
-              />
-            )) ||
-              (room?.type === 2 && (
-                <OwnerPw sendMessage={sendMessage} room={room} />
-              ))}
-          </div>,
-          startEL
-        )}
-    </>
-  );
+						{(room?.type === 3 && (
+							<OwnerInvite
+								loginer={loginer}
+								sendMessage={sendMessage}
+								participants={participants}
+							/>
+						)) ||
+							(room?.type === 2 && (
+								<OwnerPw sendMessage={sendMessage} room={room} />
+							))}
+					</div>,
+					startEL
+				)}
+		</>
+	);
 }
--- site/src/components/Chat/OwnerPw.tsx
+++ site/src/components/Chat/OwnerPw.tsx
@@ -3,126 +3,126 @@ import { UseLoginDto } from "../Log/dto/useLogin.dto";
 import { ChatRoomDto } from "src/_shared_dto/chat-room.dto";
 
 interface OwnerPwProps {
-  sendMessage: Function;
-  room: ChatRoomDto | undefined;
+	sendMessage: Function;
+	room: ChatRoomDto | undefined;
 }
 
 export default function OwnerPw({ sendMessage, room }: OwnerPwProps) {
-  const [addPassword, setAddPassword] = React.useState("");
-  const handleAddPassword = (event: SyntheticEvent) => {
-    event.preventDefault();
-    if (addPassword.length > 0) {
-      sendMessage(`/pw ${addPassword}`);
-      setAddPassword("");
-    }
-  };
+	const [addPassword, setAddPassword] = React.useState("");
+	const handleAddPassword = (event: SyntheticEvent) => {
+		event.preventDefault();
+		if (addPassword.length > 0) {
+			sendMessage(`/pw ${addPassword}`);
+			setAddPassword("");
+		}
+	};
 
-  const [currentPassword, setCurrentPassword] = React.useState("");
-  const [errorMsg, setErrorMsg] = React.useState("");
+	const [currentPassword, setCurrentPassword] = React.useState("");
+	const [errorMsg, setErrorMsg] = React.useState("");
 
-  const [newPassword, setNewPassword] = React.useState("");
-  const handleUpdatePassword = (event: SyntheticEvent) => {
-    event.preventDefault();
-    if (currentPassword.length === 0) {
-      setErrorMsg("Current password is missing");
-	  setTimeout(() => {
-		setErrorMsg("");
-	  }, 3000);
-    } else if (currentPassword.length > 0 && newPassword.length > 0) {
-      sendMessage(`/pw ${currentPassword} ${newPassword}`);
-      setCurrentPassword("");
-      setNewPassword("");
-      setErrorMsg("");
-    }
-  };
+	const [newPassword, setNewPassword] = React.useState("");
+	const handleUpdatePassword = (event: SyntheticEvent) => {
+		event.preventDefault();
+		if (currentPassword.length === 0) {
+			setErrorMsg("Current password is missing");
+			setTimeout(() => {
+				setErrorMsg("");
+			}, 3000);
+		} else if (currentPassword.length > 0 && newPassword.length > 0) {
+			sendMessage(`/pw ${currentPassword} ${newPassword}`);
+			setCurrentPassword("");
+			setNewPassword("");
+			setErrorMsg("");
+		}
+	};
 
-  const handleRemovePassword = (event: SyntheticEvent) => {
-    event.preventDefault();
-    if (currentPassword.length === 0) {
-      setErrorMsg("Current password is missing");
-	  setTimeout(() => {
-		setErrorMsg("");
-	  }, 3000);
-    } else if (currentPassword.length > 0) {
-      sendMessage(`/pw ${currentPassword}`);
-      setCurrentPassword("");
-      setErrorMsg("");
-    }
-  };
+	const handleRemovePassword = (event: SyntheticEvent) => {
+		event.preventDefault();
+		if (currentPassword.length === 0) {
+			setErrorMsg("Current password is missing");
+			setTimeout(() => {
+				setErrorMsg("");
+			}, 3000);
+		} else if (currentPassword.length > 0) {
+			sendMessage(`/pw ${currentPassword}`);
+			setCurrentPassword("");
+			setErrorMsg("");
+		}
+	};
 
-  console.log(room);
-  return (
-    <div className="w-full py-2">
-      {(room?.pw === false && (
-        <form onSubmit={handleAddPassword}>
-          <label className="mb-2">Add a password</label>
-          <div className="flex my-2 h-full w-full items-center justify-around gap-4 rounded-full shadow-lg transition duration-500 ease-in-out focus:outline-none">
-            <input
-              type="password"
-              placeholder="New password"
-              value={addPassword}
-              onChange={(event) => {
-                setAddPassword(event.target.value);
-              }}
-              className="ml-2 w-full rounded-full bg-gray-200 p-1 px-4 text-gray-600 placeholder-gray-600 focus:placeholder-gray-400 focus:outline-none"
-            />
-          </div>
-          <button
-            type="submit"
-            className="w-14 basis-1/4 rounded bg-blue-500 font-bold text-white hover:bg-blue-700"
-          >
-            Change
-          </button>
-        </form>
-      )) || (
-        <>
-          <form onSubmit={handleUpdatePassword}>
-            <label className="">Current password</label>
-            <div className="flex my-2 h-full w-full items-center justify-around gap-4 rounded-full shadow-lg transition duration-500 ease-in-out focus:outline-none">
-              <input
-                type="password"
-                placeholder="Current password"
-                value={currentPassword}
-                onChange={(event) => {
-                  setCurrentPassword(event.target.value);
-                }}
-                className="ml-2 w-full rounded-full bg-gray-200 p-1 px-4 text-gray-600 placeholder-gray-600 focus:placeholder-gray-400 focus:outline-none"
-              />
-            </div>
+	console.log(room);
+	return (
+		<div className="w-full py-2">
+			{(room?.pw === false && (
+				<form onSubmit={handleAddPassword}>
+					<label className="mb-2">Add a password</label>
+					<div className="flex my-2 h-full w-full items-center justify-around gap-4 rounded-full shadow-lg transition duration-500 ease-in-out focus:outline-none">
+						<input
+							type="password"
+							placeholder="New password"
+							value={addPassword}
+							onChange={(event) => {
+								setAddPassword(event.target.value);
+							}}
+							className="ml-2 w-full rounded-full bg-gray-200 p-1 px-4 text-gray-600 placeholder-gray-600 focus:placeholder-gray-400 focus:outline-none"
+						/>
+					</div>
+					<button
+						type="submit"
+						className="block my-2 col-span-2 mx-auto mt-3 whitespace-nowrap rounded-lg bg-cyan-500 px-4 py-2 text-center text-sm font-medium text-white hover:bg-blue-800 focus:outline-none focus:ring-4 focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+					>
+						Change
+					</button>
+				</form>
+			)) || (
+					<>
+						<form onSubmit={handleUpdatePassword}>
+							<label className="">Current password</label>
+							<div className="flex my-2 h-full w-full items-center justify-around gap-4 rounded-full shadow-lg transition duration-500 ease-in-out focus:outline-none">
+								<input
+									type="password"
+									placeholder="Current password"
+									value={currentPassword}
+									onChange={(event) => {
+										setCurrentPassword(event.target.value);
+									}}
+									className="ml-2 w-full rounded-full bg-gray-200 p-1 px-4 text-gray-600 placeholder-gray-600 focus:placeholder-gray-400 focus:outline-none"
+								/>
+							</div>
 
-            <label className="">Set a new password</label>
-            <div className="flex my-2 h-full w-full items-center justify-around gap-4 rounded-full shadow-lg transition duration-500 ease-in-out focus:outline-none">
-              <input
-                type="password"
-                placeholder="New password"
-                value={newPassword}
-                onChange={(event) => {
-                  setNewPassword(event.target.value);
-                }}
-                className="ml-2 w-full rounded-full bg-gray-200 p-1 px-4 text-gray-600 placeholder-gray-600 focus:placeholder-gray-400 focus:outline-none"
-              />
-            </div>
-            <button
-              type="submit"
-              className="block my-2 col-span-2 mx-auto mt-3 whitespace-nowrap rounded-lg bg-cyan-500 px-4 py-2 text-center text-sm font-medium text-white hover:bg-blue-800 focus:outline-none focus:ring-4 focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
-            >
-              Change
-            </button>
-          </form>
+							<label className="">Set a new password</label>
+							<div className="flex my-2 h-full w-full items-center justify-around gap-4 rounded-full shadow-lg transition duration-500 ease-in-out focus:outline-none">
+								<input
+									type="password"
+									placeholder="New password"
+									value={newPassword}
+									onChange={(event) => {
+										setNewPassword(event.target.value);
+									}}
+									className="ml-2 w-full rounded-full bg-gray-200 p-1 px-4 text-gray-600 placeholder-gray-600 focus:placeholder-gray-400 focus:outline-none"
+								/>
+							</div>
+							<button
+								type="submit"
+								className="block my-2 col-span-2 mx-auto mt-3 whitespace-nowrap rounded-lg bg-cyan-500 px-4 py-2 text-center text-sm font-medium text-white hover:bg-blue-800 focus:outline-none focus:ring-4 focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+							>
+								Change
+							</button>
+						</form>
 
-          <form onSubmit={handleRemovePassword}>
-            <label className="">Remove password</label>
-            <button
-              type="submit"
-              className="block my-2 col-span-2 mx-auto mt-3 whitespace-nowrap rounded-lg bg-cyan-500 px-4 py-2 text-center text-sm font-medium text-white hover:bg-blue-800 focus:outline-none focus:ring-4 focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
-            >
-              Remove password
-            </button>
-          </form>
+						<form onSubmit={handleRemovePassword}>
+							<label className="">Remove password</label>
+							<button
+								type="submit"
+								className="block my-2 col-span-2 mx-auto mt-3 whitespace-nowrap rounded-lg bg-cyan-500 px-4 py-2 text-center text-sm font-medium text-white hover:bg-blue-800 focus:outline-none focus:ring-4 focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+							>
+								Remove password
+							</button>
+						</form>
 
-          <div className="text-blue-600 h-1 text-center">{errorMsg}</div>
-        </>
-      )}
-    </div>
-  );
+						<div className="text-blue-600 h-1 text-center">{errorMsg}</div>
+					</>
+				)}
+		</div>
+	);
 }
