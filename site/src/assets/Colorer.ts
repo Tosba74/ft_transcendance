d1f34ed21bc621a434b9b27823644f663677a325
Jerome-JJT <47446321+Jerome-JJT@users.noreply.github.com>
2023-04-13 14:50:54 +0200

    Update Colorer.ts

--- site/src/assets/Colorer.ts
+++ site/src/assets/Colorer.ts
@@ -16,7 +16,7 @@ export default function Colorer(login: string | undefined): string {
       return "bg-red-700 dark:bg-red-900";
     case "kdi-noce":
       return "bg-[#14b8a6] dark:bg-[#7dd3fc]";
-    // Inspired by PornHub color palette
+    // Inspired by cult website color palette
     case "hsabir":
       return "bg-[#292929] dark:bg-[#ffa31a]";
     default:
