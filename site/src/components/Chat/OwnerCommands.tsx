f893b5a96a778a1cfb1fb76271b82ecbd3213098
Thomas Nanchen <thomasnanchen@hotmail.com>
2023-04-13 20:00:43 +0200

    uniformised inputs from owner cmds with starg game page inputs

--- site/src/components/Chat/OwnerCommands.tsx
+++ site/src/components/Chat/OwnerCommands.tsx
@@ -14,92 +14,92 @@ import OwnerPw from "./OwnerPw";
 const startEL = document.getElementById("root");
 
 interface OwnerCommandsProps {
-	loginer: UseLoginDto;
-	sendMessage: Function;
-	participants: ParticipantDto[];
-	role: string;
-	room: ChatRoomDto | undefined;
+  loginer: UseLoginDto;
+  sendMessage: Function;
+  participants: ParticipantDto[];
+  role: string;
+  room: ChatRoomDto | undefined;
 }
 
 export default function OwnerCommands({
-	loginer,
-	sendMessage,
-	participants,
-	role,
-	room,
+  loginer,
+  sendMessage,
+  participants,
+  role,
+  room,
 }: OwnerCommandsProps) {
-	const [portal, setPortal] = React.useState(false);
-	const [effect, setEffect] = React.useState(false);
+  const [portal, setPortal] = React.useState(false);
+  const [effect, setEffect] = React.useState(false);
 
-	const ref = React.useRef<HTMLDivElement | null>(null);
+  const ref = React.useRef<HTMLDivElement | null>(null);
 
-	const handleClickPortal = (event: SyntheticEvent) => {
-		event.preventDefault();
-		setEffect(true);
-		setPortal(true);
-	};
+  const handleClickPortal = (event: SyntheticEvent) => {
+    event.preventDefault();
+    setEffect(true);
+    setPortal(true);
+  };
 
-	React.useEffect(() => {
-		const checkIfClickedOutside = (e: any) => {
-			if (ref) {
-				if (portal && !ref.current?.contains(e.target)) {
-					setEffect(false);
-				}
-			}
-		};
+  React.useEffect(() => {
+    const checkIfClickedOutside = (e: any) => {
+      if (ref) {
+        if (portal && !ref.current?.contains(e.target)) {
+          setEffect(false);
+        }
+      }
+    };
 
-		document.addEventListener("mousedown", checkIfClickedOutside);
+    document.addEventListener("mousedown", checkIfClickedOutside);
 
-		return () => {
-			document.removeEventListener("mousedown", checkIfClickedOutside);
-		};
-	}, [portal]);
+    return () => {
+      document.removeEventListener("mousedown", checkIfClickedOutside);
+    };
+  }, [portal]);
 
-	return (
-		<>
-			<button className="pl-1" onClick={handleClickPortal}>
-				- <span className="hover:underline">{role} settings</span>
-				<svg
-					style={{ color: "white" }}
-					xmlns="http://www.w3.org/2000/svg"
-					className="ml-1 inline-block h-4 w-4"
-					viewBox="0 0 512 512"
-				>
-					<path
-						d="M256,176a80,80,0,1,0,80,80A80.24,80.24,0,0,0,256,176Zm172.72,80a165.53,165.53,0,0,1-1.64,22.34l48.69,38.12a11.59,11.59,0,0,1,2.63,14.78l-46.06,79.52a11.64,11.64,0,0,1-14.14,4.93l-57.25-23a176.56,176.56,0,0,1-38.82,22.67l-8.56,60.78A11.93,11.93,0,0,1,302.06,486H209.94a12,12,0,0,1-11.51-9.53l-8.56-60.78A169.3,169.3,0,0,1,151.05,393L93.8,416a11.64,11.64,0,0,1-14.14-4.92L33.6,331.57a11.59,11.59,0,0,1,2.63-14.78l48.69-38.12A174.58,174.58,0,0,1,83.28,256a165.53,165.53,0,0,1,1.64-22.34L36.23,195.54a11.59,11.59,0,0,1-2.63-14.78l46.06-79.52A11.64,11.64,0,0,1,93.8,96.31l57.25,23a176.56,176.56,0,0,1,38.82-22.67l8.56-60.78A11.93,11.93,0,0,1,209.94,26h92.12a12,12,0,0,1,11.51,9.53l8.56,60.78A169.3,169.3,0,0,1,361,119L418.2,96a11.64,11.64,0,0,1,14.14,4.92l46.06,79.52a11.59,11.59,0,0,1-2.63,14.78l-48.69,38.12A174.58,174.58,0,0,1,428.72,256Z"
-						fill="white">
-					</path>
-				</svg>
-			</button>
+  return (
+    <>
+      <button className="pl-1" onClick={handleClickPortal}>
+        - <span className="hover:underline">{role} settings</span>
+        <svg
+          style={{ color: "white" }}
+          xmlns="http://www.w3.org/2000/svg"
+          className="ml-1 inline-block h-4 w-4"
+          viewBox="0 0 512 512"
+        >
+          <path
+            d="M256,176a80,80,0,1,0,80,80A80.24,80.24,0,0,0,256,176Zm172.72,80a165.53,165.53,0,0,1-1.64,22.34l48.69,38.12a11.59,11.59,0,0,1,2.63,14.78l-46.06,79.52a11.64,11.64,0,0,1-14.14,4.93l-57.25-23a176.56,176.56,0,0,1-38.82,22.67l-8.56,60.78A11.93,11.93,0,0,1,302.06,486H209.94a12,12,0,0,1-11.51-9.53l-8.56-60.78A169.3,169.3,0,0,1,151.05,393L93.8,416a11.64,11.64,0,0,1-14.14-4.92L33.6,331.57a11.59,11.59,0,0,1,2.63-14.78l48.69-38.12A174.58,174.58,0,0,1,83.28,256a165.53,165.53,0,0,1,1.64-22.34L36.23,195.54a11.59,11.59,0,0,1-2.63-14.78l46.06-79.52A11.64,11.64,0,0,1,93.8,96.31l57.25,23a176.56,176.56,0,0,1,38.82-22.67l8.56-60.78A11.93,11.93,0,0,1,209.94,26h92.12a12,12,0,0,1,11.51,9.53l8.56,60.78A169.3,169.3,0,0,1,361,119L418.2,96a11.64,11.64,0,0,1,14.14,4.92l46.06,79.52a11.59,11.59,0,0,1-2.63,14.78l-48.69,38.12A174.58,174.58,0,0,1,428.72,256Z"
+            fill="white"
+          ></path>
+        </svg>
+      </button>
 
-			{portal &&
-				startEL !== null &&
-				createPortal(
-					<div
-						ref={ref}
-						className={classNames(
-							"absolute left-1/2 top-1/2 z-50 min-w-[250px] -translate-x-1/2 -translate-y-1/2 items-center rounded-lg bg-gray-100 p-6 dark:bg-gray-800 dark:text-white shadow-lg",
-							effect ? "opacity-1 animate-fadeIn" : "animate-fadeOut opacity-0"
-						)}
-						onAnimationEnd={() => {
-							if (!effect) setPortal(false);
-						}}
-					>
-						<h3 className="text-center text-xl">Owner settings</h3>
+      {portal &&
+        startEL !== null &&
+        createPortal(
+          <div
+            ref={ref}
+            className={classNames(
+              "absolute left-1/2 top-1/2 z-50 min-w-[250px] -translate-x-1/2 -translate-y-1/2 items-center rounded-lg bg-gray-100 p-6 shadow-lg dark:bg-gray-800 dark:text-white",
+              effect ? "opacity-1 animate-fadeIn" : "animate-fadeOut opacity-0"
+            )}
+            onAnimationEnd={() => {
+              if (!effect) setPortal(false);
+            }}
+          >
+            <h3 className="text-center text-xl">Owner settings</h3>
 
-						{(room?.type === 3 && (
-							<OwnerInvite
-								loginer={loginer}
-								sendMessage={sendMessage}
-								participants={participants}
-							/>
-						)) ||
-							(room?.type === 2 && (
-								<OwnerPw sendMessage={sendMessage} room={room} />
-							))}
-					</div>,
-					startEL
-				)}
-		</>
-	);
+            {(room?.type === 3 && (
+              <OwnerInvite
+                loginer={loginer}
+                sendMessage={sendMessage}
+                participants={participants}
+              />
+            )) ||
+              (room?.type === 2 && (
+                <OwnerPw sendMessage={sendMessage} room={room} />
+              ))}
+          </div>,
+          startEL
+        )}
+    </>
+  );
 }
--- site/src/components/Chat/OwnerInvite.tsx
+++ site/src/components/Chat/OwnerInvite.tsx
@@ -5,81 +5,81 @@ import { UseLoginDto } from "../Log/dto/useLogin.dto";
 import { ParticipantDto } from "src/_shared_dto/participant.dto";
 
 interface OwnerCommandsProps {
-	loginer: UseLoginDto;
-	sendMessage: Function;
-	participants: ParticipantDto[];
+  loginer: UseLoginDto;
+  sendMessage: Function;
+  participants: ParticipantDto[];
 }
 
 export default function OwnerInvite({
-	loginer,
-	sendMessage,
-	participants,
+  loginer,
+  sendMessage,
+  participants,
 }: OwnerCommandsProps) {
-	const [users, setUsers] = React.useState<ParticipantDto[]>([]);
+  const [users, setUsers] = React.useState<ParticipantDto[]>([]);
 
-	const [invite, setInvite] = React.useState("");
+  const [invite, setInvite] = React.useState("");
 
-	React.useEffect(() => {
-		axios
-			.get("/api/users", loginer.get_headers())
-			.then((res) => {
-				if (res.status === 200) {
-					const allUsers = res.data as ParticipantDto[];
-					setUsers(
-						allUsers.filter((value) => {
-							return (
-								participants.find((parti) => {
-									return parti.id === value.id;
-								}) === undefined
-							);
-						})
-					);
+  React.useEffect(() => {
+    axios
+      .get("/api/users", loginer.get_headers())
+      .then((res) => {
+        if (res.status === 200) {
+          const allUsers = res.data as ParticipantDto[];
+          setUsers(
+            allUsers.filter((value) => {
+              return (
+                participants.find((parti) => {
+                  return parti.id === value.id;
+                }) === undefined
+              );
+            })
+          );
 
-					return;
-				}
-			})
-			.catch((error) => { });
-	}, [participants]);
+          return;
+        }
+      })
+      .catch((error) => {});
+  }, [participants]);
 
-	const handleClickInvite = (event: SyntheticEvent) => {
-		event.preventDefault();
-		if (invite.length > 0) {
-			sendMessage(`/invite ${invite}`);
-			setInvite("");
-		}
-	};
+  const handleClickInvite = (event: SyntheticEvent) => {
+    event.preventDefault();
+    if (invite.length > 0) {
+      sendMessage(`/invite ${invite}`);
+      setInvite("");
+    }
+  };
 
-	return (
-		<div className="w-full py-2">
-			<label className="">Invite user</label>
-			<div className="my-2 h-full w-full items-center justify-around gap-4 rounded-full transition duration-500 ease-in-out focus:outline-none">
-				<select
-					defaultValue={""}
-					onChange={(event) => {
-						setInvite(event.target.value);
-					}}
-					className="ml-2 w-full rounded-full bg-gray-200 p-1 px-4 text-gray-600 placeholder-gray-600 focus:placeholder-gray-400 focus:outline-none"
-				>
-					<option id="defaultOption" key={-1} value="">
-						Pseudo
-					</option>
-					;
-					{users.map((user) => {
-						return (
-							<option key={user.id} value={user.id}>
-								{user.pseudo}
-							</option>
-						);
-					})}
-				</select>
+  return (
+    <div className="w-full py-2">
+      <label className="">Invite user</label>
+      <div className="my-2 h-full w-full items-center justify-around gap-4 rounded-full transition duration-500 ease-in-out focus:outline-none">
+        <select
+          defaultValue={""}
+          onChange={(event) => {
+            setInvite(event.target.value);
+          }}
+          className="w-full mx-auto rounded-lg border border-gray-300 bg-gray-50 p-1.5 text-sm text-gray-900 focus:border-blue-500 focus:ring-blue-500 dark:border-gray-600 dark:bg-gray-700 dark:text-white dark:placeholder-gray-300 dark:focus:border-blue-500 dark:focus:ring-blue-500"
+        >
+          <option id="defaultOption" key={-1} value="">
+            Pseudo
+          </option>
+          ;
+          {users.map((user) => {
+            return (
+              <option key={user.id} value={user.id}>
+                {user.pseudo}
+              </option>
+            );
+          })}
+        </select>
 
-				<button
-					className="block my-2 col-span-2 mx-auto mt-3 whitespace-nowrap rounded-lg bg-cyan-500 px-4 py-2 text-center text-sm font-medium text-white hover:bg-blue-800 focus:outline-none focus:ring-4 focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
-					onClick={handleClickInvite}
-				>
-					Invite
-				</button>
-			</div>
-		</div>
-	);
+        <button
+          className="col-span-2 my-2 mx-auto mt-3 block whitespace-nowrap rounded-lg bg-cyan-500 px-4 py-2 text-center text-sm font-medium text-white hover:bg-blue-800 focus:outline-none focus:ring-4 focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+          onClick={handleClickInvite}
+        >
+          Invite
+        </button>
+      </div>
+    </div>
+  );
 }
